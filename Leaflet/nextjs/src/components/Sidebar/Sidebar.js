6b92d1f71eb1f7722f270cfa6c49bf33da6e0995
Karlin Tchoumi <91978962+KarlTortebecker@users.noreply.github.com>
2024-04-18 14:15:47 +0100

    add sidebar

--- Leaflet/nextjs/src/components/Sidebar/Sidebar.js
+++ Leaflet/nextjs/src/components/Sidebar/Sidebar.js
@@ -1,9 +1,13 @@
-const Sidebar = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+import React from 'react';
+import styles from './Sidebar.module.scss';
+
+const Sidebar = ({ isOpen, onClose, children }) => {
+  return (
+    <div className={`${styles.sidebar} ${isOpen ? styles.open : ''}`}>
+      <button className={styles.closeButton} onClick={onClose}>Close</button>
+      {children}
+    </div>
+  );
+}
 
 export default Sidebar;
--- Leaflet/nextjs/src/pages/index.js
+++ Leaflet/nextjs/src/pages/index.js
@@ -1,24 +1,26 @@
-import { useState, useEffect } from 'react';
-import Head from 'next/head';
+import { useState, useEffect } from "react";
+import Head from "next/head";
 
-import Layout from '@components/Layout';
-import Section from '@components/Section';
-import Container from '@components/Container';
-import Map from '@components/Map';
-import SearchBar from '@components/SearchBar'; // Importez la barre de recherche ici
+import Layout from "@components/Layout";
+import Section from "@components/Section";
+import Container from "@components/Container";
+import Sidebar from "@components/Sidebar";
+import Map from "@components/Map";
+import SearchBar from "@components/SearchBar"; // Importez la barre de recherche ici
 
-import styles from '@styles/Home.module.scss';
+import styles from "@styles/Home.module.scss";
 
-import data from './markers.json';
-import regions from './regions.json';
-
+import data from "./markers.json";
+import regions from "./regions.json";
 
 const DEFAULT_CENTER = [7.37, 12.35];
 
 export default function Home() {
   const [markers, setMarkers] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const [geojsonData, setGeojsonData] = useState([]);
+  const [selectedMarker, setSelectedMarker] = useState();  
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   useEffect(() => {
     // Charger les données depuis le fichier JSON
@@ -27,65 +29,127 @@ export default function Home() {
         setMarkers(data);
         setGeojsonData(regions);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
-  }, []); 
+  }, []);
 
   // Filter markers based on searchQuery
-  const filteredMarkers = markers.filter(marker =>
-    marker.nom.toLowerCase().includes(searchQuery.toLowerCase()) ||
-    marker.localite.toLowerCase().includes(searchQuery.toLowerCase()) ||
-    marker.code.toLowerCase().includes(searchQuery.toLowerCase())
+  const filteredMarkers = markers.filter(
+    (marker) =>
+      marker.nom.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      marker.localite.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      marker.region.toLowerCase().includes(searchQuery.toLowerCase())
   );
+
+  const handleMarkerDoubleClick = (marker) => {
+    setIsSidebarOpen(true);
+    setSelectedMarker(marker);
+  };
+
+  const handleCloseSidebar = () => {
+    setSelectedMarker(null);
+    setIsSidebarOpen(false)
+  }
 
   return (
     <Layout>
       <Head>
         <title>Carte des sites d'OCM</title>
-        <meta name="description" content="Create mapping apps with Next.js Leaflet for QoE" />
+        <meta
+          name="description"
+          content="Create mapping apps with Next.js Leaflet for QoE"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Section>
         <Container>
-          <h1 className={styles.title}>
-            Carte des sites d'OCM
-          </h1>
-          
+          <h1 className={styles.title}>Carte des sites d'OCM</h1>
+
           {/* Intégration de la barre de recherche */}
           <SearchBar setSearchQuery={setSearchQuery} />
 
-          <Map className={styles.homeMap} width="1000" height="800" center={DEFAULT_CENTER} zoom={8} minZoom={5} maxZoom={13} geojsonData={geojsonData}>
-            {({ TileLayer, Marker, Popup }) => (
-              <>
-                <TileLayer
-                  url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-                />
-                
-                {/* Afficher les marqueurs filtrés */}
-                {filteredMarkers.map((marker, index) => (
-                  <Marker key={index} position={[marker.coords[0], marker.coords[1]]}>
-                    <Popup>
-                      <b>Nom du site :</b> {marker.nom} <br />
-                      <b>Localité : </b>{marker.localite} <br />
-                      <b>Code du site : </b>{marker.code}<br />
-                      <b>Type de zone : </b>{marker.Typezone}<br />
-                      <b>QoE data : </b>{marker.data}<br />
-                      <b>Zone PMO : </b>{marker.zonepmo}<br />
-                      <b>Coordonnées : </b>{marker.coords[1]} -- {marker.coords[0]}
-                    </Popup>
-                  </Marker>
-                ))}
-                
-              </>
+          {/* Afficher la carte */}
+          <div className={styles.map}>
+            <Map
+              className={styles.homeMap}
+              width="1000"
+              height="800"
+              center={DEFAULT_CENTER}
+              zoom={8}
+              minZoom={5}
+              maxZoom={13}
+              geojsonData={geojsonData}
+              onMarkerClick={(e) => handleMarkerClick(e.target)}
+            >
+              {({ TileLayer, Marker, Popup }) => (
+                <>
+                  <TileLayer url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png" />
+
+                  {/* Afficher les marqueurs filtrés */}
+                  {filteredMarkers.map((marker, index) => (
+                    <Marker
+                      key={index}
+                      position={[marker.coords[0], marker.coords[1]]}
+                      eventHandlers={{ dblclick: () => {
+                        console.log('Marker double-clicked:', marker); // Ajoutez cette ligne pour vérifier si le double-clic est capturé
+                        handleMarkerDoubleClick(marker);
+                      }}}
+                      >
+                      <Popup>
+                        <b>Nom du site :</b> {marker.nom} <br />
+                        <b>Localité : </b>
+                        {marker.localite} <br />
+                        <b>Code du site : </b>
+                        {marker.code}
+                        <br />
+                        <b>Type de zone : </b>
+                        {marker.Typezone}
+                        <br />
+                        <b>QoE data : </b>
+                        {marker.data}
+                        <br />
+                        <b>Zone PMO : </b>
+                        {marker.zonepmo}
+                        <br />
+                        <b>Coordonnées : </b>
+                        {marker.coords[1]} -- {marker.coords[0]}
+                      </Popup>
+                    </Marker>
+                  ))}
+                </>
+              )}
+            </Map>
+
+            {/* Afficher la sidebar */}
+            {isSidebarOpen && selectedMarker && (
+              <Sidebar isOpen={true} onClose={handleCloseSidebar}>
+                <h2>{selectedMarker.nom}</h2>
+                <p>Here to display the necessary to explain the QoE</p>
+                <p> Here to display the necessary to explain the QoE</p>
+                <p> Here to display the necessary to explain the QoE</p>
+                <p> Here to display the necessary to explain the QoE</p>
+                <p> Here to display the necessary to explain the QoE</p>
+                <p> Here to display the necessary to explain the QoE</p>
+                <p> Here to display the necessary to explain the QoE</p>
+                <p> Here to display the necessary to explain the QoE</p>
+                <p> Here to display the necessary to explain the QoE</p>
+                <p> Here to display the necessary to explain the QoE</p>
+                <p> Here to display the necessary to explain the QoE</p>
+                <p> Here to display the necessary to explain the QoE</p>
+              </Sidebar>
             )}
-          </Map>
+              
+            </div>
+
 
           <p className={styles.description}>
-            <code className={styles.code}>Premiers résultats de l'affichage des sites sur la carte</code>
+            <code className={styles.code}>
+              Premiers résultats de l'affichage des sites sur la carte
+            </code>
           </p>
         </Container>
       </Section>
