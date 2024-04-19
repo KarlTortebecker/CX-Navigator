a36e2d311449acadde4c4e44360e234a7e4c4b99
Karlin Tchoumi <91978962+KarlTortebecker@users.noreply.github.com>
2024-04-19 12:45:56 +0100

    Ajouter la sélection de vue

--- Leaflet/nextjs/src/pages/index.js
+++ Leaflet/nextjs/src/pages/index.js
@@ -14,11 +14,13 @@ import data from "./markers.json";
 import regions from "./regions.json";
 
 const DEFAULT_CENTER = [7.37, 12.35];
+const views = ["Pays", "Régions", "Départements", "Arrondissements", "Sites"];
 
 export default function Home() {
   const [markers, setMarkers] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [geojsonData, setGeojsonData] = useState([]);
+  const [selectedView, setSelectedView] = useState(views[4]);
   const [selectedMarker, setSelectedMarker] = useState();  
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
@@ -52,7 +54,12 @@ export default function Home() {
   const handleCloseSidebar = () => {
     setSelectedMarker(null);
     setIsSidebarOpen(false)
-  }
+  };
+
+  // Fonction pour gérer le changement de vue
+  const handleChangeView = (event) => {
+    setSelectedView(event.target.value);
+  };
 
   return (
     <Layout>
@@ -72,12 +79,25 @@ export default function Home() {
           {/* Intégration de la barre de recherche */}
           <SearchBar setSearchQuery={setSearchQuery} />
 
+          {/* Sélecteur de vue */}
+          <div className={styles.sidebar}>
+            <select value={selectedView} onChange={handleChangeView}>
+              {views.map((view) => (
+                <option key={view} value={view}>
+                  {view}
+                </option>
+              ))}
+            </select>
+          </div>
+
+
           {/* Afficher la sidebar */}
           {isSidebarOpen && selectedMarker && (
               <Sidebar isOpen={true} onClose={handleCloseSidebar}>
-                <h2 className={styles.code}>{selectedMarker.nom}</h2>
+                <h2 className={styles.code}>Site de {selectedMarker.nom}</h2>
                 <p> Localité :  {selectedMarker.localite}</p>
-                <p> Type de zone : {selectedMarker.zonepmo}</p>
+                <p> Zone PMO :  {selectedMarker.zonepmo}</p>
+                <p> Type de zone : {selectedMarker.Typezone}</p>
                 <p> Région : {selectedMarker.region}</p>
                 <p> Département : {selectedMarker.departement}</p>
                 <p> Arrondissement : {selectedMarker.arrondissement}</p>
@@ -92,7 +112,61 @@ export default function Home() {
           {/* Afficher la carte */}
           <div className={styles.map}>
               
-            <Map
+            {/* Affichage en fonction de la vue sélectionnée */}
+            {selectedView === "Sites" && (
+              // Afficher les sites sur la carte
+              <Map
+              className={styles.homeMap}
+              width="1000"
+              height="800"
+              center={DEFAULT_CENTER}
+              zoom={8}
+              minZoom={7}
+              maxZoom={11}
+            >
+            
+              {({ TileLayer, Marker, Popup }) => (
+                <>
+                  <TileLayer url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png" />
+
+                  {/* Afficher les marqueurs filtrés */}
+                  {filteredMarkers.map((marker, index) => (
+                    <Marker
+                      key={index}
+                      position={[marker.coords[0], marker.coords[1]]}
+                      eventHandlers={{ dblclick: () => {
+                       handleMarkerDoubleClick(marker);
+                      }}}
+                      >
+                      <Popup>
+                        <b>Nom du site :</b> {marker.nom} <br />
+                        <b>Localité : </b>
+                        {marker.localite} <br />
+                        <b>Code du site : </b>
+                        {marker.code}
+                        <br />
+                        <b>Type de zone : </b>
+                        {marker.Typezone}
+                        <br />
+                        <b>QoE data : </b>
+                        {marker.data}
+                        <br />
+                        <b>Zone PMO : </b>
+                        {marker.zonepmo}
+                        <br />
+                        <b>Coordonnées : </b>
+                        {marker.coords[1]} -- {marker.coords[0]}
+                      </Popup>
+                    </Marker>
+                  ))}
+                </>
+              )}
+            </Map>
+            )}
+              
+              {selectedView === "Régions" && (
+              // Afficher les sites sur la carte
+              <Map
               className={styles.homeMap}
               width="1000"
               height="800"
@@ -104,7 +178,7 @@ export default function Home() {
               onMarkerClick={(e) => handleMarkerClick(e.target)}
             >
             
-              {({ TileLayer, Marker, Popup, Sidebar }) => (
+              {({ TileLayer, Marker, Popup }) => (
                 <>
                   <TileLayer url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png" />
 
@@ -142,8 +216,7 @@ export default function Home() {
                 </>
               )}
             </Map>
-              
-              
+            )}
 
             </div>
 
